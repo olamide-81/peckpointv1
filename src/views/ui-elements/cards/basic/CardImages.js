9d8c14ed5b7325afad57cae43b7ae13eb204f892
george-hub331 <idiaghegeorge9@gmail.com>
2022-04-17 22:59:29 +0100

    hmm...check out the birthday page

--- src/views/ui-elements/cards/basic/CardImages.js
+++ src/views/ui-elements/cards/basic/CardImages.js
@@ -1,7 +1,9 @@
 // ** React Imports
-import { Fragment, React, useRef } from 'react'
+import { Fragment, React, useRef, useState } from 'react'
 //import html2canvas from 'html2canvas'
 import html2canvas from 'html2canvas'
+
+import axios from 'axios'
 
 // ** Reactstrap Imports
 import { Card, CardImg, CardTitle, CardBody, CardImgOverlay, CardText, Row, Col, Button } from 'reactstrap'
@@ -16,6 +18,9 @@ import img4 from '@src/assets/images/avatars/7.png'
 const CardImages = () => {
   
   const user = JSON.parse(localStorage.getItem('user'))
+  const token = user.token
+  
+
   const printRef = useRef()
 
   const handleDownloadImage = async () => {
@@ -36,12 +41,52 @@ const CardImages = () => {
       window.open(data)
     }
   }
+  const [conts, setConts] = useState([])
 
+  axios.get('https://api.peckpoint.com/api/v1/contacts', {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json'
+    }
+  }).then(res => {
+    setConts(res.data.data)    
+  })
+  // console.log(conts)
   return (
     <Fragment>
       <Row>
+
+        {
+          conts.map((e, i) => ( 
+            <Col xl='6' key={i} md='6'>
+              <Card className='birthdaycardf' responsive="true">
+                <div className='birthdaycardf' ref={printRef}>
+                  <CardBody className='birthdaycard'>
+                    <CardTitle className='birthdaycard'>
+                      <img src={img1} className='baloon' />
+                      <div className='section-2'>
+                        <img src={img2} className='happy-birthday' />
+                        <div>
+                          <img src={img4} className='profile-picture' />
+                          <h2>{(`${e.firstname} ${e.lastname}`).toUpperCase()}</h2>
+                          <p>- With Love from {user.user.name}</p>
+                        </div>
+                      </div>
+                      <img src={img3} className='gift' />
+                    </CardTitle>
+                  </CardBody>
+                </div>
+              </Card>
+              <Button color='primary' outline className='button' onClick={handleDownloadImage}>
+                Download
+              </Button>
+            </Col>
+          ))
+        }
+        
+
         <Col xl='6' md='6'>
-          <Card className='birthdaycardf' responsive>
+          <Card className='birthdaycardf' responsive="true">
             <div className='birthdaycardf' ref={printRef}>
             <CardBody className='birthdaycard'>
               <CardTitle className='birthdaycard'> 
@@ -64,7 +109,7 @@ const CardImages = () => {
             </Button>
         </Col>
         <Col xl='6' md='6'>
-          <Card className='birthdaycardf' responsive>
+          <Card className='birthdaycardf' responsive="true">
             <CardBody className='birthdaycard'>
               <CardTitle className='birthdaycard'> 
                 <img src={img1} className='baloon'/> 
@@ -85,7 +130,7 @@ const CardImages = () => {
             </Button>
         </Col>
         <Col xl='6' md='6'>
-          <Card className='birthdaycardf' responsive>
+          <Card className='birthdaycardf' responsive="true">
             <CardBody className='birthdaycard'>
               <CardTitle className='birthdaycard'> 
                 <img src={img1} className='baloon'/> 
@@ -101,12 +146,12 @@ const CardImages = () => {
                 </CardTitle>
             </CardBody>
           </Card>
-          <Button color='primary' outline className='button'>
+          <Button color='primary' onClick={handleDownloadImage} outline className='button'>
               Download
             </Button>
         </Col>
         <Col xl='6' md='6'>
-          <Card className='birthdaycardf' responsive>
+          <Card className='birthdaycardf' responsive="true">
             <CardBody className='birthdaycard'>
               <CardTitle className='birthdaycard'> 
                 <img src={img1} className='baloon'/> 
