127b228db5eb0e43cb26bdb3ce4444573002b99b
george-hub331 <idiaghegeorge9@gmail.com>
2022-04-25 18:39:03 +0100

    ...

--- src/views/pages/authentication/VerifyEmailBasic.js
+++ src/views/pages/authentication/VerifyEmailBasic.js
@@ -1,11 +1,45 @@
 // ** React Imports
 import { Link } from 'react-router-dom'
 
+import axios from 'axios'
+
 // ** Reactstrap Imports
 import { Card, CardBody, CardTitle, CardText, Button } from 'reactstrap'
 
 // ** Styles
 import '@styles/react/pages/page-authentication.scss'
+
+
+const getGet = (val) => {
+  const str = (window.location.search).substring(1)
+
+  const ee = str.split('&')
+
+  for (const e in ee) {
+    const c = ee[e].split('=')
+    if (c[0] === val) {
+      return c[1]
+    }
+  }
+  return null
+}
+  const tok = getGet('token')
+  let status = {
+    status : false,
+    message : 'Loading...'
+  }
+  if (tok === null) {
+      window.location = '/login'
+}
+axios.post('https://api.peckpoint.com/api/v2/verify-account', {
+  data: {
+    token : tok
+  }
+}).then(res => {
+  status = res.data
+})
+
+console.log(tok)
 
 const VerifyEmailBasic = () => {
   return (
@@ -17,10 +51,10 @@ const VerifyEmailBasic = () => {
               <h2 className='brand-text text-primary ms-1'>PeckPoint</h2>
             </Link>
             <CardTitle tag='h2' className='fw-bolder mb-1'>
-              Success ✉️
+              {status.status ? 'Success ✉️' : (status.message === 'Loading...' ? 'Waiting' : 'Failed')}
             </CardTitle>
             <CardText className='mb-2'>
-             Email was verified successfully !
+             { status.status ? 'Email was verified successfully !' : status.message}
             </CardText>
             <Button block tag={Link} to='/' color='primary'>
               Go back to Login
