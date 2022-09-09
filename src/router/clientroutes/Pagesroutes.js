eae88142b604760e9d0f6ca80fae6abfd4ce7ffb
george-hub331 <idiaghegeorge9@gmail.com>
2022-09-09 15:18:30 +0100

    update: verifyemail route

--- src/router/clientroutes/Pagesroutes.js
+++ src/router/clientroutes/Pagesroutes.js
@@ -96,12 +96,12 @@ const PagesRoutes = [
     layout: 'BlankLayout'
   },
   {
-    path: '/pages/verify-email-basic',
+    path: '/verifyemail',
     component: lazy(() => import('../../peckpoint-pages/authentication/VerifyEmailBasic')),
     layout: 'BlankLayout'
   },
   {
-    path: '/verifyemail',
+    path: '/pages/verify-email-basic',
     component: lazy(() => import('../../views/pages/authentication/VerifyEmailCover')),
     layout: 'BlankLayout',
     meta: {
